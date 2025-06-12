5d554ccbb4eb1645928d2d8fd415626c5be8c8c5
Arshi <mohanapriyaarumugam662@gmail.com>
2025-06-12 02:38:55 -0700

    maven-example

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a = 5;
+        int b = 5;
+        System.out.println("Sum of a and b is:"+(a+b));
+    }
+}
